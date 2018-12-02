dc080060e012ac5135fbda3a8905402c53db051f
Tomek Josiak <tomek.josiak@gmail.com>
2018-12-02 09:36:54 +0100

    Add Promethee and GAIAplane methods

---
+++ Promethee/Promethee.R
@@ -0,0 +1,219 @@
+
+
+
+calculatePreference <- function(
+    performanceTable,
+    criteriaPreferenceFunctions
+) {
+    preferenceCube <- array(
+        0, 
+        dim=c(
+            nrow(performanceTable),
+            nrow(performanceTable),
+            ncol(performanceTable)
+        ),
+        dimnames = list(
+            rownames(performanceTable),
+            rownames(performanceTable),
+            colnames(performanceTable)
+        )
+    )
+    for (c in seq(ncol(performanceTable))) {
+        criterionPreferenceFunction <- criteriaPreferenceFunctions[[c]]
+
+        for (i in seq(nrow(performanceTable))) {
+            for (j in seq(nrow(performanceTable))) {
+                if (i == j) next
+                preferenceCube[i, j, c] <- 
+                    criterionPreferenceFunction(performanceTable[i, c], performanceTable[j, c])
+            }
+        }
+    }
+    return(preferenceCube)
+}
+
+calculateCriteriaPositiveFlows <- function(preferenceCube) {
+    alternativesLength <- dim(preferenceCube)[1]
+
+    apply(preferenceCube, c(1, 3), function(alernativeAgainsOther) {
+        sum(alernativeAgainsOther)/(alternativesLength - 1)
+    })
+}
+
+calculateCriteriaNegativeFlows <- function(preferenceCube) {
+     alternativesLength <- dim(preferenceCube)[1]
+
+    apply(preferenceCube, c(2, 3), function(othersAgainstAlternative) {
+        sum(othersAgainstAlternative)/(alternativesLength - 1)
+    })
+}
+
+calculateCriteriaNetFlows <- function(preferenceCube) {
+    calculateCriteriaPositiveFlows(preferenceCube) - calculateCriteriaNegativeFlows(preferenceCube)
+}
+
+calculateAggregatedPreference <- function(preferenceCube, criteriaWeights) {
+    
+    alternativesLength <- dim(preferenceCube)[1]
+    criteriaLength <- dim(preferenceCube)[3]
+
+    criteriaWeightsSum <- sum(criteriaWeights)
+    
+    apply(preferenceCube, c(1, 2), function(criteria) {
+        sum(criteria*criteriaWeights)/criteriaWeightsSum
+    })
+}
+
+mapPositiveOutrankingFlow <- function(aggregatedPreferenceTable) {
+    numberOfAlternatives <- dim(aggregatedPreferenceTable)[1]
+
+    apply(aggregatedPreferenceTable, 1, function(row) {
+        sum(row) / (numberOfAlternatives - 1)
+    })
+}
+
+mapNegativeOutrankingFlow <- function(aggregatedPreferenceTable) {
+    numberOfAlternatives <- dim(aggregatedPreferenceTable)[1]
+    
+    apply(aggregatedPreferenceTable, 2, function(row) {
+        sum(row) / (numberOfAlternatives - 1)
+    })
+}
+
+
+PrometheeI <- function(
+    performanceTable,
+    criteriaPreferenceFunction,
+    criteriaWeights
+) {
+    numberOfAlternatives <- dim(performanceTable)[1]
+    preferenceCube <- calculatePreference(performanceTable, criteriaPreferenceFunction)
+    aggregatedPreferenceTable <- calculateAggregatedPreference(preferenceCube, criteriaWeights)
+    positiveFlow <- mapPositiveOutrankingFlow(aggregatedPreferenceTable)
+    negativeFlow <- mapNegativeOutrankingFlow(aggregatedPreferenceTable)
+
+    
+    flow <- cbind(positiveFlow, negativeFlow)
+    
+    ranking <- array('_', dim=c(numberOfAlternatives, numberOfAlternatives))
+
+    print(aggregatedPreferenceTable)
+    print(flow)
+
+    for (i in seq(numberOfAlternatives)) {
+        for (k in seq(numberOfAlternatives)) {
+            positiveI <- positiveFlow[i]
+            positiveK <- positiveFlow[k]
+            negativeI <- negativeFlow[i]
+            negativeK <- negativeFlow[k]
+
+            if (positiveI > positiveK) {
+                if (negativeI <= negativeK) {
+                    ranking[i, k] = 'P'
+                } else {
+                    ranking[i, k] = 'R'
+                }
+            } else if (negativeI < negativeK) {
+                if (positiveI >= positiveK) {
+                    ranking[i, k] = 'P'
+                } else {
+                    ranking[i, k] = 'R'
+                }
+            } else if (positiveI == positiveK && negativeI == negativeK) {
+                ranking[i, k] = 'I'
+            }
+        }
+    }
+
+    ranking
+}
+
+PrometheeII <- function(
+    performanceTable,
+    criteriaPreferenceFunction,
+    criteriaWeights
+) {
+    numberOfAlternatives <- dim(performanceTable)[1]
+    preferenceCube <- calculatePreference(performanceTable, criteriaPreferenceFunction)
+    aggregatedPreferenceTable <- calculateAggregatedPreference(preferenceCube, criteriaWeights)
+    positiveFlow <- mapPositiveOutrankingFlow(aggregatedPreferenceTable)
+    negativeFlow <- mapNegativeOutrankingFlow(aggregatedPreferenceTable)
+
+    flow <- positiveFlow + negativeFlow
+
+    ranking <- array('_', dim=c(numberOfAlternatives, numberOfAlternatives))
+
+    for (i in seq(numberOfAlternatives)) {
+        for (k in seq(numberOfAlternatives)) {
+            if (flow[i] > flow[k]) {
+                ranking[i, k] = 'P'
+            } else if (flow[i] == flow[k]) {
+                ranking[i, k] = 'I'
+            }
+        }
+    }
+
+    ranking
+}
+
+get_criteria_difference <- function (a_criterion_value, b_criterion_value, criterion_gain) {
+    criteria_difference <- a_criterion_value - b_criterion_value
+    if (!criterion_gain) {
+        criteria_difference <- -1 * criteria_difference 
+    }
+    criteria_difference
+}
+
+criteriaFunctions = list()
+criteriaFunctions[["Usual"]] = function (criterion_gain=TRUE) { 
+    function(a_criterion_value, b_criterion_value) {
+        criteria_difference <- get_criteria_difference(a_criterion_value, b_criterion_value, criterion_gain)
+        
+        if (criteria_difference > 0) return(1)
+        return(0)
+    }
+}
+criteriaFunctions[["U-shape"]] = function (q, criterion_gain=TRUE) { 
+    function(a_criterion_value, b_criterion_value) {
+        criteria_difference <- get_criteria_difference(a_criterion_value, b_criterion_value, criterion_gain)
+        
+        if (criteria_difference > q) return(1)
+        return(0)
+    }
+}
+criteriaFunctions[["V-shape"]] = function (p, criterion_gain=TRUE) { 
+    function(a_criterion_value, b_criterion_value) {
+        criteria_difference <- get_criteria_difference(a_criterion_value, b_criterion_value, criterion_gain)
+
+        if (criteria_difference > p) return(1)
+        if (criteria_difference <= 0) return(0)
+        return(criteria_difference/p)
+    }
+}
+criteriaFunctions[["V-shape-with-incriteria_difference"]] = function (q, p, criterion_gain=TRUE) { 
+    function(a_criterion_value, b_criterion_value) {
+        criteria_difference <- get_criteria_difference(a_criterion_value, b_criterion_value, criterion_gain)
+        
+        if (criteria_difference < q) return(0)
+        if (criteria_difference > p) return(1)
+        return((criteria_difference - q) / (p - q))
+    }
+}
+criteriaFunctions[["Level"]] = function (q, p, criterion_gain=TRUE) { 
+    function(a_criterion_value, b_criterion_value) {
+        criteria_difference <- get_criteria_difference(a_criterion_value, b_criterion_value, criterion_gain)
+        
+        if (criteria_difference <= q) return(0)
+        if (criteria_difference > p) return(1)
+        return(0.5)
+    }
+}
+criteriaFunctions[["Gaussian"]] = function (s, criterion_gain=TRUE) { 
+    function(a_criterion_value, b_criterion_value) {
+        criteria_difference <- get_criteria_difference(a_criterion_value, b_criterion_value, criterion_gain)
+        
+        if (criteria_difference <= 0) return(0)
+        squared <- criteria_difference * criteria_difference
+        return (1 - exp(-1*squared/(2*s*s)))
+    }
+}
